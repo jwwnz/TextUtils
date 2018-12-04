edb838adfc88ce1be46f28fade12664d5a940590
jwwnz <jwon117@aucklanduni.ac.nz>
2018-12-04 23:12:50 +0900

    Update console application to run logic.

--- WordCounterApp/Program.cs
+++ WordCounterApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TextUtils;
 
 namespace WordCounterApp
 {
@@ -6,7 +7,21 @@ namespace WordCounterApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a search word:");
+            var searchWord = Console.ReadLine();
+            Console.WriteLine("Provide a string to search:");
+            var inputString = Console.ReadLine();
+
+            var wordCount = WordCount.GetWordCount(searchWord, inputString);
+
+            var pluralChar = "s";
+            if (wordCount == 1)
+            {
+                pluralChar = string.Empty;
+            }
+
+            Console.WriteLine($"The search word {searchWord} appears " +
+                              $"{wordCount} time{pluralChar}.");
         }
     }
 }
